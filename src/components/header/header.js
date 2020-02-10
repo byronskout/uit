79f3963a97c4537e47f509666be91a5de8ff6b8e
byronskout <vyskoutaris@hotmail.com>
2020-02-10 21:28:15 +0000

    Header.js

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.scss';
+
+const Header = (props) => {
+    return (
+        <header className="headerComponent">
+            <div className="wrap">
+                    Header
+            </div>
+        </header>
+    )
+};
+
+export default Header;
